a7f8de43d2d9c4428a419b73462635c7164571e6
jhonatanjcdg <j3172224877@gmail.com>
2024-12-09 22:33:30 -0500

    agrego todo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,33 @@
-const { join } = require('path');
-
-const forms = require('@tailwindcss/forms');
-const typography = require('@tailwindcss/typography');
-const { skeleton } = require('@skeletonlabs/tw-plugin');
-
-module.exports = {
-    darkMode: 'class',
-    content: [
-        './src/**/*.{html,js,svelte,ts}',
-        join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-    ],
-    theme: {
-        extend: {}
-    },
-    plugins: [
-        forms,
-        typography,
-        skeleton({
-            themes: {
-                preset: [
-                    {
-                        name: 'skeleton',
-                        enhancements: false
-                    }
-                ]
-            }
-        })
-    ],
-    darkMode: 'selector' 
-};
+(function() {
+    const { join } = require('path');
+    
+    const forms = require('@tailwindcss/forms');
+    const typography = require('@tailwindcss/typography');
+    const { skeleton } = require('@skeletonlabs/tw-plugin');
+    
+    module.exports = {
+        darkMode: 'class',
+        content: [
+            './src/**/*.{html,js,svelte,ts}',
+            join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+        ],
+        theme: {
+            extend: {}
+        },
+        plugins: [
+            forms,
+            typography,
+            skeleton({
+                themes: {
+                    preset: [
+                        {
+                            name: 'skeleton',
+                            enhancements: false
+                        }
+                    ]
+                }
+            })
+        ],
+        darkMode: 'selector' 
+    };
+})();
