85f30144f876eafccf33755b7480adb70014b855
jhonatanjcdg <j3172224877@gmail.com>
2024-12-09 22:28:07 -0500

    agrego todo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,35 @@
 import { join } from 'path';
+import { fileURLToPath } from 'url';
 
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..');
+
 export default {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: false
-					}
-				]
-			}
-		})
-	],
-	darkMode: 'selector' // <--
+    darkMode: 'class',
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        join(await import.meta.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [
+        forms,
+        typography,
+        skeleton({
+            themes: {
+                preset: [
+                    {
+                        name: 'skeleton',
+                        enhancements: false
+                    }
+                ]
+            }
+        })
+    ],
+    darkMode: 'selector' 
 };
